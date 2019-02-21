75627586655ff55071f1df0664327643a3f9e384
Eugene Ar <ar@ars-macbookpro.local>
2019-02-21 19:20:31 +0300

    Init commit

---
+++ One_two/Controller/ViewController.swift
@@ -0,0 +1,144 @@
+//
+//  ViewController.swift
+//  One_two
+//
+//  Created by Eugene Ar on 21/02/2019.
+//  Copyright © 2019 Kin. All rights reserved.
+//
+
+import UIKit
+import Contacts
+import ContactsUI
+
+class ViewController: UIViewController, CNContactPickerDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIDocumentPickerDelegate {
+    @IBOutlet weak var qrCodeImageView: UIImageView!
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var roleLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var mailLabel: UILabel!
+    @IBOutlet weak var siteLabel: UILabel!
+    @IBOutlet weak var contactButton: UIButton!
+    @IBOutlet weak var logoButtonView: UIView!
+    @IBOutlet weak var infoStackView: UIStackView!
+    
+    let businessCard = BusinessCard()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.updateView()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
+    func updateView() {
+
+        self.qrCodeImageView.isHidden = !businessCard.isFilled
+        self.infoStackView.isHidden = !businessCard.isFilled
+        self.contactButton.isHidden = businessCard.isFilled
+        
+        // Updating all labels and their hideness from businessCard
+        // Hide label if it's empty
+        self.nameLabel.text = self.businessCard.name
+        self.nameLabel.isHidden = self.businessCard.name == ""
+
+        self.roleLabel.text = self.businessCard.role
+        self.roleLabel.isHidden = self.businessCard.role == ""
+        
+        self.phoneLabel.text = self.businessCard.phone
+        self.phoneLabel.isHidden = self.businessCard.phone == ""
+        
+        self.mailLabel.text = self.businessCard.mail
+        self.mailLabel.isHidden = self.businessCard.mail == ""
+        
+        self.siteLabel.text = self.businessCard.site
+        self.siteLabel.isHidden = self.businessCard.site == ""
+        
+        self.qrCodeImageView.image = self.businessCard.qr
+    }
+    
+    // MARK - contacts
+    
+    func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
+        print("some contact picked")
+        self.dismiss(animated: true, completion: nil)
+        
+        self.businessCard.updateContactInfo(using: contact)
+        self.updateView()
+    }
+    
+    @IBAction func setContactPressed(_ sender: UIButton) {
+        
+        func openContacts() {
+            let contactPicker = CNContactPickerViewController.init()
+            contactPicker.delegate = self
+            
+            self.present(contactPicker, animated: true, completion: nil)
+        }
+        
+        // Configuring contacts
+        let entityType = CNEntityType.contacts
+        let authStatus = CNContactStore.authorizationStatus(for: entityType)
+        if authStatus == CNAuthorizationStatus.notDetermined {
+            let contactsStore = CNContactStore.init()
+            contactsStore.requestAccess(for: entityType, completionHandler: { (success, nil) in
+                if success {
+                    openContacts()
+                }
+                else {
+                    print("Not authorized")
+                }
+            })
+        }
+        else if authStatus == CNAuthorizationStatus.authorized {
+            openContacts()
+        }
+    }
+    
+    // MARK - logo
+    
+    @IBAction func addLogoButtonPressed(_ sender: UIButton) {
+        self.openLibrary()
+        //TODO - should add taking photos from Documents
+    }
+    
+    func openLibrary() {
+        // Open PhotoLibrary to take picture for logo
+        let logoImage = UIImagePickerController()
+        logoImage.sourceType = .photoLibrary
+        logoImage.allowsEditing = false
+        logoImage.delegate = self
+        self.present(logoImage, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                // Make it exactly the logoImageView height
+                let height = CGFloat(self.logoImageView.frame.height)
+                let ratio = height/image.size.height
+                let width = CGFloat(image.size.width * ratio)
+                let rect = CGRect(x: 0, y: 0, width: width, height: height)
+                
+                UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: height), false, 1.0)
+                image.draw(in: rect)
+                let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+                UIGraphicsEndImageContext()
+                
+                // Hide block with button and show imageView with logo
+                self.logoButtonView.isHidden = true
+                self.logoImageView.isHidden = false
+                
+                self.logoImageView.image = resizedImage
+                // update Logo in default storage
+                self.businessCard.updateLogo(using: resizedImage!)
+            }
+            else {
+                //Error message
+            }
+            self.dismiss(animated: true, completion: nil)
+    }
+}
+
---
+++ One_two/Model/BusinessCard.swift
@@ -0,0 +1,141 @@
+//
+//  File.swift
+//  One_two
+//
+//  Created by Eugene Ar on 21/02/2019.
+//  Copyright © 2019 Kin. All rights reserved.
+//
+
+import Foundation
+import Contacts
+import UIKit
+
+// A class to store and save info from card
+class BusinessCard {
+    var name: String
+    var role: String
+    var phone: String
+    var mail: String
+    var site: String
+    var qr: UIImage
+    var logo: UIImage
+    var isFilled = false
+    
+    struct keys {
+        static let name = "name"
+        static let role = "role"
+        static let phone = "phone"
+        static let mail = "mail"
+        static let site = "site"
+        static let qr = "qr"
+        static let logo = "logo"
+        static let isEmpty = "isEmpty"
+    }
+    
+    init() {
+        if let  name = UserDefaults.standard.string(forKey: keys.name) {
+            self.name = name
+        } else { self.name = "" }
+        
+        if let role = UserDefaults.standard.string(forKey: keys.role) {
+            self.role = role
+        } else { role = "" }
+        
+        if let phone = UserDefaults.standard.string(forKey: keys.phone) {
+            self.phone = phone
+        } else { phone = "" }
+        
+        if let mail = UserDefaults.standard.string(forKey: keys.mail) {
+            self.mail = mail
+        } else { mail = "" }
+        
+        if let site = UserDefaults.standard.string(forKey: keys.site) {
+            self.site = site
+        } else { site = "" }
+        
+        if let qr = UserDefaults.standard.data(forKey: keys.qr) {
+            self.qr = UIImage(data: qr)!
+        } else { qr = UIImage() }
+
+        if let logo = UserDefaults.standard.data(forKey: keys.logo) {
+            self.logo = UIImage(data: logo)!
+        } else { logo = UIImage() }
+        
+        let isFilled = UserDefaults.standard.bool(forKey: keys.isEmpty)
+        self.isFilled = isFilled
+    }
+    
+    func updateContactInfo(using contact: CNContact) {
+        // Update texts
+        let name = "\(contact.givenName) \(contact.familyName)"
+        UserDefaults.standard.set(name, forKey: keys.name)
+        self.name = name
+        
+        let role = contact.jobTitle
+        UserDefaults.standard.set(role, forKey: keys.role)
+        self.role = role
+        
+        if let phone = contact.phoneNumbers.first?.value.stringValue {
+            UserDefaults.standard.set(phone, forKey: keys.phone)
+            self.phone = phone
+        }
+    
+        if let mail = contact.emailAddresses.first?.value.decomposedStringWithCanonicalMapping {
+            UserDefaults.standard.set(mail, forKey: keys.mail)
+            self.mail = mail
+        }
+
+        if let site = contact.urlAddresses.first?.value.decomposedStringWithCanonicalMapping {
+            UserDefaults.standard.set(site, forKey: keys.site)
+            self.site = site
+        }
+        
+        // Update filled switcher
+        self.isFilled = true
+        UserDefaults.standard.set(self.isFilled, forKey: keys.isEmpty)
+        
+        // Update QR-code
+        do {
+            let data = try CNContactVCardSerialization.data(with: [contact])
+            if let image = generateQRCode(from: data) {
+                // Updating info at default storage
+                self.qr = image
+                let qrData = image.pngData()
+                UserDefaults.standard.set(qrData, forKey: keys.qr)
+            } else {
+                print("Cannot generate QR-code")
+            }
+        } catch {
+            print("Cannot get contact data: \(error)")
+        }
+    }
+    
+    func generateQRCode(from data: Data) -> UIImage? {
+        // Making QR-code from CIFilter
+        let img: UIImage
+        
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            filter.setValue("M", forKey: "inputCorrectionLevel")
+            let transform = CGAffineTransform(scaleX: 4, y: 4)
+            
+            img = UIImage(ciImage: (filter.outputImage?.transformed(by: transform))!)
+            
+            UIGraphicsBeginImageContextWithOptions(img.size, false, img.scale)
+            img.draw(in: CGRect(origin: .zero, size: img.size))
+            guard let redraw = UIGraphicsGetImageFromCurrentImageContext() else { return nil }
+            
+            return redraw
+        }
+        print("Cannot create filter")
+        return nil
+    }
+    
+    func updateLogo(using image: UIImage) {
+        // Update Logo
+        self.logo = image
+        let qrData = image.pngData()
+        UserDefaults.standard.set(qrData, forKey: keys.logo)
+    }
+}
+
